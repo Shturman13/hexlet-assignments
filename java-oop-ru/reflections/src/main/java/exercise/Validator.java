5cc4880ae670350a2ce648f9c168f7082ef3ed16
@hexlet/cli <support@hexlet.io>
2025-01-17 20:31:05 +0100

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -12,16 +12,16 @@ public class Validator {
     public static List<String> validate(Address address) {
         var notPassedValidation = new ArrayList<String>();
         for (Field field : address.getClass().getDeclaredFields()) {
-           if (field.isAnnotationPresent(NotNull.class)) {
-               field.setAccessible(true);
-               try {
-                   if (field.get(address) == null) {
+            if (field.isAnnotationPresent(NotNull.class)) {
+                field.setAccessible(true);
+                try {
+                    if (field.get(address) == null) {
                        notPassedValidation.add(field.getName());
-                   }
-               } catch (IllegalAccessException e) {
-                   e.printStackTrace();
-               }
-           }
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return notPassedValidation;
     }
@@ -30,38 +30,38 @@ public class Validator {
         var listOfErrors = new HashMap<String, List<String>>();
         for (Field field : address.getClass().getDeclaredFields()) {
             field.setAccessible(true);
-                try {
-                    var value = field.get(address);
-                    if (field.isAnnotationPresent(NotNull.class)) {
-                        if (value == null) {
-                            listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
-                                    .add("can not be null");
+            try {
+                var value = field.get(address);
+                if (field.isAnnotationPresent(NotNull.class)) {
+                    if (value == null) {
+                        listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
+                                .add("can not be null");
 
 
-                        } else {
-                            if (field.isAnnotationPresent(MinLength.class)) {
-                                MinLength minLengthAnnotation = field.getAnnotation(MinLength.class);
-                                int minLength = minLengthAnnotation.minLength();
-                                if (value instanceof String &&  ((String) value).length() < minLength) {
-                                    listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>()).add("length less than " + minLength);
-                                }
+                    } else {
+                        if (field.isAnnotationPresent(MinLength.class)) {
+                            MinLength minLengthAnnotation = field.getAnnotation(MinLength.class);
+                            int minLength = minLengthAnnotation.minLength();
+                            if (value instanceof String &&  ((String) value).length() < minLength) {
+                                listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>()).add("length less than " + minLength);
                             }
-                        }
-
-                    } else if (field.isAnnotationPresent(MinLength.class)) {
-                        // Если поле не @NotNull, но имеет @MinLength, проверяем только на длину
-                        MinLength minLengthAnnotation = field.getAnnotation(MinLength.class);
-                        int minLength = minLengthAnnotation.minLength();
-                        if (value instanceof String && ((String) value).length() < minLength) {
-                            listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
-                                    .add("length less than " + minLength);
                         }
                     }
 
-
-                } catch (IllegalAccessException e) {
-                            e.printStackTrace();
+                } else if (field.isAnnotationPresent(MinLength.class)) {
+                        // Если поле не @NotNull, но имеет @MinLength, проверяем только на длину
+                    MinLength minLengthAnnotation = field.getAnnotation(MinLength.class);
+                    int minLength = minLengthAnnotation.minLength();
+                    if (value instanceof String && ((String) value).length() < minLength) {
+                        listOfErrors.computeIfAbsent(field.getName(), k -> new ArrayList<>())
+                                .add("length less than " + minLength);
+                    }
                 }
+
+
+            } catch (IllegalAccessException e) {
+                            e.printStackTrace();
+            }
         }
         return listOfErrors;
     }
